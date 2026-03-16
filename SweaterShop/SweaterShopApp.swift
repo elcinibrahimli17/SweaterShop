2dec5624ca7acb691afca3dd5f29cf629035cbcb
Elcin <elcinibrahimli17@gmail.com>
2026-03-16 22:39:46 +0400

    Initial Commit

---
+++ SweaterShop/SweaterShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopApp.swift
+//  SweaterShop
+//
+//  Created by Elchın on 16.03.26.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
